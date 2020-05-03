52a974211fed25beb66ac25f80a944623341ae04
@lilo$oft <fellahi.ali@gmail.com>
2020-05-03 19:10:25 +0100

    skeleton ready, start migration of old code

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,33 +1,16 @@
 <template>
   <div id="app">
     <Main msg="Welcome" />
-    <button @click="fetchRelevant">Relevant</button>
-    <ol>
-      <li v-for="(repo, index) in relevantRepoNames" :key="index">
-        {{ repo }}
-      </li>
-    </ol>
   </div>
 </template>
 
 <script>
   import Main from './components/Main.vue'
-  import firebase from '../data/firebase'
+  //import firebase from '../data/firebase'
   export default {
     user: 'App',
     components: {
       Main
-    },
-    data() {
-      return {
-        relevantRepoNames: []
-      }
-    },
-    methods: {
-      async fetchRelevant() {
-        const repos = await firebase.getRelevantRepos()
-        this.relevantRepoNames = Object.values(repos)
-      }
     }
   }
 </script>
--- vue-app/src/components/Main.vue
+++ vue-app/src/components/Main.vue
@@ -1,32 +1,23 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
-</template>
+<template src="./template.html"> </template>
+<style src="./style.css"></style>
 
 <script>
+  import firebase from '../../data/firebase'
   export default {
     name: 'Main',
     props: {
       msg: String
+    },
+    data() {
+      return {
+        relevantRepo: []
+      }
+    },
+    methods: {
+      async fetchRelevant() {
+        const repos = await firebase.getRelevantRepos()
+        this.relevantRepo = Object.values(repos)
+      }
     }
   }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-</style>
