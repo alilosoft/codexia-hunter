f0a341cf006c4a472d839f9507d1a601ee164b6f
@lilo$oft <fellahi.ali@gmail.com>
2020-05-03 15:59:40 +0100

    bring codexia scripts

---
+++ vue-app/codexia/focus.md
@@ -0,0 +1,7 @@
+# We are specifically interested in
+
+- Tools for developers
+- Software development frameworks
+- Instruments for cloud computing
+- Static analyzers and code quality checkers
+- AI/ML frameworks
---
+++ vue-app/codexia/submit.js
@@ -0,0 +1,43 @@
+const axios = require("axios").default; // CommonJS
+const firebase = require("../data/firebase");
+const API_TOKEN = require("../config/secrets").CODEXIA_TOKEN;
+const DateTime = require("luxon").DateTime;
+const today = DateTime.local().toISODate();
+
+async function submitAll() {
+  const repos = await firebase.getRelevantRepos();
+  const reposAsArray = Object.values(repos);
+  console.log("relevant repos count: " + reposAsArray.length);
+  for (const repo of reposAsArray) {
+    await submit(repo);
+  }
+  console.log("saving submitted...");
+  await firebase.saveSubmittedRepos(repos, today);
+  firebase.db().goOffline();
+}
+
+submitAll().catch(console.error);
+
+//************************************ */
+// TODO: use axios create
+const config = {
+  headers: {
+    "X-Codexia-Token": API_TOKEN,
+  },
+};
+
+// todo: use axios create
+async function submit(repo) {
+  let url =
+    "https://www.codexia.org/submit?platform=github&coordinates=" + repo;
+
+  await axios
+    .post(url, {}, config)
+    .then((r) => console.log(repo + " -> " + r.statusText))
+    .catch((e) => {
+      console.log("Error submitting " + repo);
+      console.log(e);
+    });
+}
+
+//submit("test");
