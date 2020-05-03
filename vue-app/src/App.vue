f945d5879355a198423379b89314b0b2aec89943
@lilo$oft <fellahi.ali@gmail.com>
2020-05-03 15:21:33 +0100

    move App.vue style to separate file

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -15,13 +15,4 @@
     }
 </script>
 
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
+<style src="./style.css"></style>
