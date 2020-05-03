edcc47032c3b601cbdd5761f1018c30e1bb18a4b
@lilo$oft <fellahi.ali@gmail.com>
2020-05-03 19:14:34 +0100

    data copied from main.js

--- vue-app/src/components/Main.vue
+++ vue-app/src/components/Main.vue
@@ -10,6 +10,20 @@
     },
     data() {
       return {
+        notifType: {
+          INFO: { backgroundColor: '#64dd17' },
+          WARN: { backgroundColor: '#ff9800' },
+          ERROR: { backgroundColor: 'crimson', color: 'white' }
+        },
+        notifications: [], // {message: "", color: color}
+        query: 'stars:100..10000 pushed:>2019-05-01',
+        page: 1,
+        per_page: 25,
+        total_count: 0,
+        gh_repos: [],
+        sel_repos_names: [],
+        sel_topics: [],
+        submitted_repos: [],
         relevantRepo: []
       }
     },
