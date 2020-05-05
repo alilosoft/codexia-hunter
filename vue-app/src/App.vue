30d80c62e9c2440e0689962f1839aa2bb6cf1174
@lilo$oft <fellahi.ali@gmail.com>
2020-05-05 20:34:36 +0100

    move style to App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -49,4 +49,12 @@
   }
 </script>
 
-<style src="./style.css"></style>
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 2px;
+  }
+</style>
