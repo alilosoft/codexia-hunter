2a5f7ae1c0bb4bd51629cf2c690a4853cf0b4ec4
@lilo$oft <fellahi.ali@gmail.com>
2020-05-07 20:53:01 +0100

    delete sponsored projects

---
+++ vue-app/codexia/delete.js
@@ -0,0 +1,59 @@
+const firebase = require('../data/firebase')
+
+function shouldDelete(proj) {
+  const bigCompany = [
+    'facebook',
+    'google',
+    'microsoft',
+    'dotnet',
+    'ibm',
+    'oracle',
+    'intel',
+    'alibaba',
+    'aws',
+    'pivotal',
+    'mozilla',
+    'uber',
+    'netflix',
+    'spring',
+    'apache',
+    'azure'
+  ]
+  return bigCompany.some(company => proj.coordinates.startsWith(company))
+}
+// console.log('google? ' + shouldDelete({ coordinates: 'google' }))
+// console.log('facebook? ' + shouldDelete({ coordinates: 'facebook' }))
+// console.log('foo? ' + shouldDelete({ coordinates: 'foo' }))
+
+async function toDelete(filter = shouldDelete) {
+  const submitted = await firebase.getSubmittedRepos()
+  firebase.db().goOffline()
+
+  const toDelete = submitted
+    .filter(shouldDelete)
+    .filter(p => (p.submitter = 'codexia-hunter'))
+  return toDelete
+}
+
+async function deleteAll() {
+  const all = await toDelete()
+  let count = all.length
+  console.log('to delete count: ' + count)
+  for (p of all) {
+    console.log(count-- + ' > ' + ' delete: ' + p.coordinates)
+    await deleteProj(p.id)
+  }
+}
+
+deleteAll()
+
+// delete from codexia
+async function deleteProj(id) {
+  await new Promise(res => {
+    setTimeout(() => {
+      console.log('deleting...: ' + id)
+      // TODO: waiting for #135
+      res()
+    }, 500)
+  })
+}
